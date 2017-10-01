16c7197b259de503d7c685ad55c9d547ce1dec1b
tam94131 <del@tahl.net>
2017-10-01 15:47:05 -0700

    almost\!

--- js/app.js
+++ js/app.js
@@ -28,6 +28,8 @@ function clearBoard() {
 	sayWhosTurn();
 }
 
+// $(button).on("click",clearBoard);
+
 function checkPlayer(a) {
 	if ((b[0]===a && b[1]===a && b[2]===a) ||
 		(b[3]===a && b[4]===a && b[5]===a) || 
